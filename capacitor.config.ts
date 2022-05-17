28b4ac946a7ff66c80dd8ca21b32952d0696fb1f
CesarAAR <cealalvarezr@ittepic.edu.mx>
2022-05-17 16:47:33 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'T3Sports',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
